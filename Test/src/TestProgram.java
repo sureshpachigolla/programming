b64b4755e2dc8c5726a5952995e079b11024233b
Sneha <suresh.pachigolla@gmail.com>
2018-12-07 20:36:47 -0600

    clearing the master

---
+++ Test/src/TestProgram.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		
+		int[] salary;
+		
+		salary = new int[10];
+		
+		
+		
+		
+	
+		
+		System.out.println(salary[1]);
+	}
+
+}
